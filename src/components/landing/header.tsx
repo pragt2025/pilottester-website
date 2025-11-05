58478f894bf4124bef97a3866728208cd1403bd3
Sparkbrain AI <sparkbrainai2024@gmail.com>
2025-11-05 11:37:31 +0000

    There is no gap in logo of left side of navigation menu. Please fix ths

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -33,7 +33,7 @@ export function Header() {
           </nav>
         </div>
 
-        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+        <div className="flex flex-1 items-center justify-start md:justify-end">
           <div className="md:hidden">
             <Sheet>
               <SheetTrigger asChild>
